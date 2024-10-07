4090f0f755920d9ba91c043cdb17bc94183eabd1
gauss963 <gauss963@gmail.com>
2024-10-07 13:06:14 +0800

    Finish task 1 (Plotting events with zise)

--- code/epicentral.f90
+++ code/epicentral.f90
@@ -11,6 +11,10 @@
     
     real :: ax_x_max, ax_x_min, ax_y_max, ax_y_min, margen
 
+    real :: EQ_M_min, EQ_M_max
+    real :: size_min, size_max
+    real :: symbol_size
+
     ! Get contour data -------------------------------------------------------------------
     open(newunit = unit_num, file = "../data/Taiwan.txt", status = "old", action = "read")
     n = 0
@@ -60,6 +64,12 @@
     ax_y_min = minval(CONTOUR_Y) - margen
     ax_y_max = maxval(CONTOUR_Y) + margen
 
+    EQ_M_min = minval(EQ_Y)
+    EQ_M_max = maxval(EQ_Y)
+    size_min = 0.5
+    size_max = 2.0
+
+
     ! Plot Contour
     call pgopen('1999_event_distribution.ps/VCPS')
     call pgsci(1)
@@ -68,14 +78,22 @@
     call pglab('Longitude (E)', 'Latitude (N)', '1999 Event Distribution')
     call pgline(n, CONTOUR_X, CONTOUR_Y)
     
-    
-    call pgmove(0, 0)
+
+    ! Plot symbol size with "EQ_Y", chahge to EQ_M later
     call pgsci(2)
-    call pgpt(n, EQ_X, EQ_Y, 5)
+    do i = 1, n
+        symbol_size = size_min + (EQ_Y(i) - EQ_M_min) * (size_max - size_min) / (EQ_M_max - EQ_M_min)
+        
+        call pgsch(symbol_size)
+        
+        call pgpt1(EQ_X(i), EQ_Y(i), 22)
+    end do
+    call pgsch(1.0)
     
     
     
     call pgclos()
+
 
     deallocate(CONTOUR_X, CONTOUR_Y)
     deallocate(EQ_X, EQ_Y, EQ_Z, EQ_M)

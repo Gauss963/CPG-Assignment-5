35694219fb7e9a41feb80a0688b63bfe510572f2
gauss963 <gauss963@gmail.com>
2024-10-14 19:01:59 +0800

    Finish data reading

--- code/VPDistribution.f90
+++ code/VPDistribution.f90
@@ -8,10 +8,12 @@ program VPDistribution
     integer :: io_status, unit_num
     real :: x, y, z, w
 
-    real, allocatable :: EQ_X(:), EQ_Y(:), EQ_Z(:), EQ_M(:)
+    real, allocatable :: Distance(:), Depth(:)
+    real, allocatable :: Vp(:), Vp_perturbation(:)
+    real, allocatable :: Long(:), Lat(:)
 
     ! Get earthquake data ----------------------------------------------------------------
-    open(newunit = unit_num, file = "../data/1999.lis", status = "old", action = "read")
+    open(newunit = unit_num, file = "../data/Vp_prof.dat", status = "old", action = "read")
     n = 0
     do
         read(unit_num, '(A)', iostat=io_status) line
@@ -19,18 +21,29 @@ program VPDistribution
         n = n + 1
     end do
     close(unit_num)
-    allocate(EQ_X(n), EQ_Y(n), EQ_Z(n), EQ_M(n))
+    allocate(Distance(n), Depth(n))
+    allocate(Vp(n), Vp_perturbation(n))
+    allocate(Long(n), Lat(n))
 
-    open(newunit=unit_num, file = "../data/1999.lis", status = "old", action = "read")
+    open(newunit=unit_num, file = "../data/Vp_prof.dat", status = "old", action = "read")
+    read(unit_num, *, iostat=io_status)
     do i = 1, n
-        read(unit_num, '(18X, F2.0, F5.2, F3.0, F5.2, F5.2)', iostat=io_status) x, y, z, w, EQ_Z(i)
-        EQ_X(i) = (z + w / 60)
-        EQ_Y(i) = (x + y / 60)
+        read(unit_num, '(2X, F8.4, 2X, F8.4, 2X, F8.4, 2X, F10.5, F8.4, 2X, F8.4)', iostat=io_status) &
+        Distance(i), Depth(i), Vp(i), Vp_perturbation(i), Long(i), Lat(i)
 
 
         if (io_status /= 0) exit
     end do
     close(unit_num)
 
+
+
+    print *, Distance(1), Depth(1), Vp(1), Vp_perturbation(1), Long(1), Lat(1)
+
+    ! Finish data reading
+    
+    deallocate(Distance, Depth)
+    deallocate(Vp, Vp_perturbation)
+    deallocate(Long, Lat)
 
 end program VPDistribution
